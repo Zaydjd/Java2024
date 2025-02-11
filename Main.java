0d7090712822f069a46c006fc8e3824c5f4c0c9c
Zaid <jdouazaid@gmail.com>
2025-02-11 16:00:57 +0100

    depot

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    run|Debug
+    public static void main (string[] args){
+        voiture voiture = new Voiture();
+        voiture.marque = "Toyota";
+        voiture.modele = "Corolla";
+        voiture.couleur = "Rouge";
+
+        voiture.demarrer();
+}
