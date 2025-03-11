c83a5aa690802c32f6ef9f9f98167e80516638d7
Zaydjd <jdouazaid@gmail.com>
2025-03-11 17:17:29 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
-public class Main {
-    run|Debug
-    public static void main (string[] args){
-        voiture voiture = new Voiture();
-        voiture.marque = "Toyota";
-        voiture.modele = "Corolla";
-        voiture.couleur = "Rouge";
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-        voiture.demarrer();
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Bienvenue dans JavaFX !");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Première application JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
